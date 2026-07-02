4f031ced04399f1514f064ea462458e74e02a6e4
maga.dev <acostamr97@gmail.com>
2026-07-02 01:00:01 -0300

    test-deploy

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -25,7 +25,7 @@ export const HomeScreen = () => {
     )
 
     return (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-red-400">
             <Navbar />
 
             <div className="max-w-3xl mx-auto px-6 py-10">
